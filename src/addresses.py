ab4a5da28a5e685191beeaab7190182983db5d56
Jonathan Warren <git@jonwarren.org>
2013-05-15 18:03:33 -0400

    Bugfix: Must use toAddress instead of addressInKeysFile if toLabel is blank when receiving message

--- src/addresses.py
+++ src/addresses.py
@@ -276,7 +276,7 @@ if __name__ == "__main__":
     print 'Ripe digest that we will encode in the address:', ripe.digest().encode('hex')
     returnedAddress = encodeAddress(addressVersionNumber,streamNumber,ripe.digest())
     print 'Encoded address:', returnedAddress
-    status,addressVersionNumber,streamNumber,data = decodeAddress(returnedAddress)
+    status,addressVersionNumber,streamNumber,data = decodeAddress("oriDZJ7VAEjphwQmt7fACFqZJVwds26vW")
     print '\nAfter decoding address:'
     print 'Status:', status
     print 'addressVersionNumber', addressVersionNumber
