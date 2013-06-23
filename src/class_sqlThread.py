f1d2b042eacc004410504cb6976c7d1f4978c5f4
Jonathan Warren <me@jonwarren.org>
2013-06-23 02:38:21 -0400

    add import sys to class_sqlThread.py

--- src/class_sqlThread.py
+++ src/class_sqlThread.py
@@ -3,6 +3,7 @@ import shared
 import sqlite3
 import time
 import shutil  # used for moving the messages.dat file
+import sys
 
 # This thread exists because SQLITE3 is so un-threadsafe that we must
 # submit queries to it and it puts results back in a different queue. They
