9c5104d2e1d88d62d5c28b79c92f0dd49d8c01cd
Jordan Hall <jordan@hall05.co.uk>
2013-06-30 22:57:35 +0100

    Refactoring various SQL into helper_sent.py

--- src/helper_sent.py
+++ src/helper_sent.py
@@ -9,3 +9,34 @@ def insert(t):
     shared.sqlSubmitQueue.put('commit')
     shared.sqlLock.release()
     
+def updateStatusByAckData(t):
+    shared.sqlLock.acquire()
+    shared.sqlSubmitQueue.put(
+        'UPDATE sent SET status=? WHERE ackdata=?')
+    shared.sqlSubmitQueue.put(t)
+    shared.sqlReturnQueue.get()
+    shared.sqlSubmitQueue.put('commit')
+    shared.sqlLock.release()
+    
+def updateStatusForPossibleNewPubKey(t):
+    shared.sqlLock.acquire()
+    shared.sqlSubmitQueue.put(
+        '''UPDATE sent SET status='doingmsgpow' WHERE toripe=? AND (status='awaitingpubkey' or status='doingpubkeypow') and folder='sent' ''')
+    shared.sqlSubmitQueue.put(t)
+    shared.sqlReturnQueue.get()
+    shared.sqlSubmitQueue.put('commit')
+    shared.sqlLock.release()
+    
+def updateStatusForResend(t):
+    shared.sqlSubmitQueue.put(
+        '''UPDATE sent SET lastactiontime=?, msgretrynumber=?, status=? WHERE ackdata=?''')
+    shared.sqlSubmitQueue.put(t)
+    shared.sqlReturnQueue.get()
+    shared.sqlSubmitQueue.put('commit')
+    
+def updateStatusForRerequestPubKey(t):
+    shared.sqlSubmitQueue.put(
+        '''UPDATE sent SET lastactiontime=?, pubkeyretrynumber=?, status='msgqueued' WHERE toripe=?''')
+    shared.sqlSubmitQueue.put(t)
+    shared.sqlReturnQueue.get()
+    shared.sqlSubmitQueue.put('commit')
