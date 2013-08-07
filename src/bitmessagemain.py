ce3f97c0230d34cc8d84b6468abe0d6003567de1
Jordan Hall <jordan@hall05.co.uk>
2013-08-07 21:34:46 +0100

    Fix to python version critical log message

--- src/bitmessagemain.py
+++ src/bitmessagemain.py
@@ -40,7 +40,7 @@ from debug import logger
 import sys
 if sys.platform == 'darwin':
     if float("{1}.{2}".format(*sys.version_info)) < 7.5:
-        logger.critical("You should use python 2.7.5 or greater. Your version: %s", format(*sys.version_info))
+        logger.critical("You should use python 2.7.5 or greater. Your version: %s", "{0}.{1}.{2}".format(*sys.version_info))
         sys.exit(0)
 
 def connectToStream(streamNumber):
