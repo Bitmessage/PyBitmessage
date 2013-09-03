8447c23881f0489ac69ff00a753aebd98eae545a
Jonathan Warren <me@jonwarren.org>
2013-09-03 00:30:48 -0400

    minor changes to previous commit- adding listAddressBook to API

--- src/bitmessagemain.py
+++ src/bitmessagemain.py
@@ -171,19 +171,15 @@ class MySimpleXMLRPCRequestHandler(SimpleXMLRPCRequestHandler):
                                        streamNumber, 'enabled': shared.config.getboolean(addressInKeysFile, 'enabled')}, indent=4, separators=(',', ': '))
             data += ']}'
             return data
-        elif method == 'listAddressbook':
-            shared.sqlLock.acquire()
-            shared.sqlSubmitQueue.put('''SELECT label, address from addressbook''')
-            shared.sqlSubmitQueue.put('')
-            queryreturn = shared.sqlReturnQueue.get()
-            shared.sqlLock.release()
+        elif method == 'listAddressBook' or method == 'listAddressbook':
+            queryreturn = sqlQuery('''SELECT label, address from addressbook''')
             data = '{"addresses":['
             for row in queryreturn:
                 label, address = row
                 label = shared.fixPotentiallyInvalidUTF8Data(label)
                 if len(data) > 20:
                     data += ','
-                data += json.dumps({'label':label.encode('base64'), 'address': address})
+                data += json.dumps({'label':label.encode('base64'), 'address': address}, indent=4, separators=(',', ': '))
             data += ']}'
             return data
         elif method == 'createRandomAddress':
