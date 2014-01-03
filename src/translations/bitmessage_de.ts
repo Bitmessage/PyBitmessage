5cdf4a9630abd378d31b9594f369bba9fdb88302
flaskevann <pppperppp@hotmail.com>
2014-01-03 21:22:47 +0100

    Update bitmessage_de.ts
    Changed "norsk" to "Norwegian" at line 1681.

--- src/translations/bitmessage_de.ts
+++ src/translations/bitmessage_de.ts
@@ -1678,7 +1678,7 @@ Die Zufallszahlen-Option ist standard, jedoch haben deterministische Adressen ei
     </message>
     <message>
         <location filename="../bitmessageqt/settings.py" line="401"/>
-        <source>norsk</source>
+        <source>Norwegian</source>
         <comment>no</comment>
         <translation></translation>
     </message>
