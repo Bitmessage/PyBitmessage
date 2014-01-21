ed7769203ae2dc18aad83612893dbb68791fe58c
Jonathan Warren <me@jonwarren.org>
2014-01-21 01:17:36 -0500

    minor change to new function isOurOperatingSystemLimitedToHavingVeryFewHalfOpenConnections

--- src/helper_startup.py
+++ src/helper_startup.py
@@ -126,7 +126,7 @@ def isOurOperatingSystemLimitedToHavingVeryFewHalfOpenConnections():
     try:
         VER_THIS=StrictVersion(platform.version())
         if sys.platform[0:3]=="win":
-            return StrictVersion("5.1.2600")<VER_THIS and StrictVersion("6.0.6000")>VER_THIS
+            return StrictVersion("5.1.2600")<=VER_THIS and StrictVersion("6.0.6000")>=VER_THIS
         return False
     except Exception as err:
         print 'An Exception occurred within isOurOperatingSystemLimitedToHavingVeryFewHalfOpenConnections:', err
