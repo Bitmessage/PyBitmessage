b41fb616ae831de17c113bdc6545bb4a4ebeabb1
Jonathan Warren <me@jonwarren.org>
2014-07-26 13:15:28 -0400

    ignore duplicate messages

--- src/helper_inbox.py
+++ src/helper_inbox.py
@@ -9,3 +9,7 @@ def trash(msgid):
     sqlExecute('''UPDATE inbox SET folder='trash' WHERE msgid=?''', msgid)
     shared.UISignalQueue.put(('removeInboxRowByMsgid',msgid))
     
+def isMessageAlreadyInInbox(toAddress, fromAddress, subject, body, encodingType):
+    queryReturn = sqlQuery(
+        '''SELECT COUNT(*) FROM inbox WHERE toaddress=? AND fromaddress=? AND subject=? AND message=? AND encodingtype=? ''', toAddress, fromAddress, subject, body, encodingType)
+    return queryReturn[0][0] != 0
