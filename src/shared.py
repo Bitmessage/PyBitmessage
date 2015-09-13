f1a9274d138800e34ca55daaf0df463be0fc2c02
Kamil Rytarowski <n54@gmx.com>
2015-09-13 23:30:05 +0200

    Add proper filesystem check for NetBSD
    FreeBSD and NetBSD are compatible here.

--- src/shared.py
+++ src/shared.py
@@ -423,8 +423,8 @@ def checkSensitiveFilePermissions(filename):
         # TODO: This might deserve extra checks by someone familiar with
         # Windows systems.
         return True
-    elif sys.platform[:7] == 'freebsd':
-        # FreeBSD file systems are the same as major Linux file systems
+    elif sys.platform[:7] == 'freebsd' or sys.platform[:6] == 'netbsd':
+        # The BSD file systems are the same as major Linux file systems
         present_permissions = os.stat(filename)[0]
         disallowed_permissions = stat.S_IRWXG | stat.S_IRWXO
         return present_permissions & disallowed_permissions == 0
