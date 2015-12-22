e5675987a760fc0996f8c77e6be677e151d0613d
mailchuck <dev@mailchuck.com>
2015-12-23 00:34:41 +0100

    Fix frozen localisation (OSX)
    Fixes Bitmessage#737

--- src/build_osx.py
+++ src/build_osx.py
@@ -1,18 +1,27 @@
 import os
+from PyQt4 import QtCore
 from setuptools import setup
 
 name = "Bitmessage"
 version = os.getenv("PYBITMESSAGEVERSION", "custom")
 mainscript = ["bitmessagemain.py"]
 
+DATA_FILES = [
+    ('', ['sslkeys', 'images']),
+    ('bitmsghash', ['bitmsghash/bitmsghash.cl', 'bitmsghash/bitmsghash.so']),
+    ('translations', glob('translations/*.qm')),
+    ('translations', glob(str(QtCore.QLibraryInfo.location(QtCore.QLibraryInfo.TranslationsPath)) + '/qt_??.qm')),
+    ('translations', glob(str(QtCore.QLibraryInfo.location(QtCore.QLibraryInfo.TranslationsPath)) + '/qt_??_??.qm')),
+]
+
 setup(
     name = name,
     version = version,
     app = mainscript,
+    data_files=DATA_FILES
     setup_requires = ["py2app"],
     options = dict(
         py2app = dict(
-            resources = ["images", "translations", "bitmsghash", "sslkeys"],
             includes = ['sip', 'PyQt4._qt'],
             iconfile = "images/bitmessage.icns"
         )
