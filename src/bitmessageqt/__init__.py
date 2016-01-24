42e0f7ba36fed7241b7742df3fe1e5e07acd19b7
mailchuck <dev@mailchuck.com>
2016-01-24 01:16:00 +0100

    Disabling subscriptions should rerender addressbook
    Addresses #76

--- src/bitmessageqt/__init__.py
+++ src/bitmessageqt/__init__.py
@@ -3325,7 +3325,7 @@ class MyForm(settingsmixin.SMainWindow):
             address)
         account = self.getCurrentItem()
         account.setEnabled(False)
-        self.rerenderAddressBook(address)
+        self.rerenderAddressBook()
         shared.reloadBroadcastSendersForWhichImWatching()
 
     def on_context_menuSubscriptions(self, point):
