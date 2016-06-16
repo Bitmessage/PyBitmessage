4245c8a811a4072e1420929bf0bc4c8f870a226a
Peter Surda <surda@economicsofbitcoin.com>
2016-06-16 14:06:36 +0200

    Fix UTF8 character in translation source
    - "About" dialog did not correctly interpret source UTF-8 characters

--- src/bitmessageqt/about.py
+++ src/bitmessageqt/about.py
@@ -23,6 +23,9 @@ except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
 
+def __trUtf8(context, text):
+    return _translate(context, text, None)
+
 class Ui_aboutDialog(object):
     def setupUi(self, aboutDialog):
         aboutDialog.setObjectName(_fromUtf8("aboutDialog"))
@@ -66,7 +69,7 @@ class Ui_aboutDialog(object):
         aboutDialog.setWindowTitle(_translate("aboutDialog", "About", None))
         self.label.setText(_translate("aboutDialog", "PyBitmessage", None))
         self.labelVersion.setText(_translate("aboutDialog", "version ?", None))
-        self.label_2.setText(_translate("aboutDialog", "<html><head/><body><p>Copyright © 2012-2016 Jonathan Warren<br/>Copyright © 2013-2016 The Bitmessage Developers</p></body></html>", None))
+        self.label_2.setText(__trUtf8("aboutDialog", "<html><head/><body><p>Copyright © 2012-2016 Jonathan Warren<br/>Copyright © 2013-2016 The Bitmessage Developers</p></body></html>"))
         self.label_3.setText(_translate("aboutDialog", "<html><head/><body><p>Distributed under the MIT/X11 software license; see <a href=\"http://www.opensource.org/licenses/mit-license.php\"><span style=\" text-decoration: underline; color:#0000ff;\">http://www.opensource.org/licenses/mit-license.php</span></a></p></body></html>", None))
         self.label_5.setText(_translate("aboutDialog", "This is Beta software.", None))
 
