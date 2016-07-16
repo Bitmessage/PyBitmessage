e18926c74a263115120d360fc9a993ba8a7b5791
ng0 <ng0@we.make.ritual.n0.is>
2016-07-16 15:42:19 +0000

    Fix python shebangs from `#!/usr/bin/env python2.7*' to `#!/usr/bin/python2.7'.
    * src/bitmessagecli.py: fix it.
    * src/bitmessagemain.py: same.
    
    This fixes #887.

--- src/bitmessagecli.py
+++ src/bitmessagecli.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7.x
+#!/usr/bin/python2.7
 # Created by Adam Melton (.dok) referenceing https://bitmessage.org/wiki/API_Reference for API documentation
 # Distributed under the MIT/X11 software license. See http://www.opensource.org/licenses/mit-license.php.
 
