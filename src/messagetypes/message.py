ac7182c1b5dbf7d888eb8d7ab51767e7e91dcb59
Peter Surda <surda@economicsofbitcoin.com>
2016-11-15 17:10:33 +0100

    Add extended encoding message validation
    - validates variables of the "message" type

--- src/messagetypes/message.py
+++ src/messagetypes/message.py
@@ -1,13 +1,21 @@
 from debug import logger
 from messagetypes import MsgBase
 
+
 class Message(MsgBase):
     def __init__(self):
         return
 
     def decode(self, data):
-        self.subject = data["subject"]
-        self.body = data["body"]
+        # UTF-8 and variable type validator
+        if type(data["subject"]) is str:
+            self.subject = unicode(data["subject"], 'utf-8', 'replace')
+        else:
+            self.subject = unicode(str(data["subject"]), 'utf-8', 'replace')
+        if type(data["body"]) is str:
+            self.body = unicode(data["body"], 'utf-8', 'replace')
+        else:
+            self.body = unicode(str(data["body"]), 'utf-8', 'replace')
 
     def encode(self, data):
         super(Message, self).encode()
