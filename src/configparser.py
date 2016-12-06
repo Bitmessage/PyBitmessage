176879ce659e9d9f32a5ab5826aa7d3e9f85c656
Peter Surda <surda@economicsofbitcoin.com>
2016-12-06 13:04:52 +0100

    Fix timeformat in configparser
    - allow both raw (new) and non-raw (old) style timeformat for backwards
      compatibility

--- src/configparser.py
+++ src/configparser.py
@@ -1,15 +1,20 @@
-from ConfigParser import SafeConfigParser, ConfigParser
+import ConfigParser
 
-class BMConfigParser(SafeConfigParser):
+class BMConfigParser(ConfigParser.SafeConfigParser):
     def set(self, section, option, value=None):
         if self._optcre is self.OPTCRE or value:
             if not isinstance(value, basestring):
                 raise TypeError("option values must be strings")
-        return ConfigParser.set(self, section, option, value)
+        return ConfigParser.ConfigParser.set(self, section, option, value)
 
     def get(self, section, option, raw=False, vars=None):
-        return ConfigParser.get(self, section, option, True, vars)
+        if section == "bitmessagesettings" and option == "timeformat":
+            try: 
+                return ConfigParser.ConfigParser.get(self, section, option, raw, vars)
+            except ConfigParser.InterpolationError:
+                return ConfigParser.ConfigParser.get(self, section, option, True, vars)
+        return ConfigParser.ConfigParser.get(self, section, option, True, vars)
 
     def items(self, section, raw=False, vars=None):
-        return ConfigParser.items(self, section, True, vars)
+        return ConfigParser.ConfigParser.items(self, section, True, vars)
 
