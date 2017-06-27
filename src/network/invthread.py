cc3cf777596f43be63310daeae804bd640b015f9
Peter Surda <surda@economicsofbitcoin.com>
2017-06-27 13:25:12 +0200

    New class multiqueue
    - to be used for invthread and addthread
    - updated invthread for multiqueue

--- src/network/invthread.py
+++ src/network/invthread.py
@@ -1,37 +1,22 @@
-from binascii import hexlify
-import collections
 import Queue
-import random
 import threading
-import time
 
 import addresses
-from bmconfigparser import BMConfigParser
-from debug import logger
 from helper_threading import StoppableThread
-from network.bmproto import BMProto
 from network.connectionpool import BMConnectionPool
 from queues import invQueue
 import protocol
 import state
 
 class InvThread(threading.Thread, StoppableThread):
-    size = 10
-
     def __init__(self):
         threading.Thread.__init__(self, name="InvThread")
         self.initStop()
         self.name = "InvThread"
 
-        self.shutdown = False
-
-        self.collectionOfInvs = []
-        for i in range(InvThread.size):
-            self.collectionOfInvs.append({})
-
     def run(self):
-        iterator = 0
         while not state.shutdown:
+            chunk = []
             while True:
                 try:
                     data = invQueue.get(False)
@@ -39,50 +24,25 @@ class InvThread(threading.Thread, StoppableThread):
                         BMConnectionPool().handleReceivedObject(data[0], data[1])
                     else:
                         BMConnectionPool().handleReceivedObject(data[0], data[1], data[2])
-                    self.holdHash (data[0], data[1])
+                    chunk.append((data[0], data[1]))
                 except Queue.Empty:
                     break
 
-            if self.collectionOfInvs[iterator]:
-                for connection in BMConnectionPool().inboundConnections.values() + BMConnectionPool().outboundConnections.values():
+            if chunk:
+                for connection in BMConnectionPool().inboundConnections.values() + \
+                        BMConnectionPool().outboundConnections.values():
                     hashes = []
-                    for stream in connection.streams:
+                    for inv in chunk:
+                        if inv[0] not in connection.streams:
+                            continue
                         try:
-                            for hashId in self.collectionOfInvs[iterator][stream]:
-                                try:
-                                    with connection.objectsNewToThemLock:
-                                        del connection.objectsNewToThem[hashId]
-                                    hashes.append(hashId)
-                                except KeyError:
-                                    pass
+                            with connection.objectsNewToThemLock:
+                                del connection.objectsNewToThem[inv[1]]
+                            hashes.append(inv[1])
                         except KeyError:
                             continue
                     if hashes:
-                        connection.writeQueue.put(protocol.CreatePacket('inv', addresses.encodeVarint(len(hashes)) + "".join(hashes)))
-                self.collectionOfInvs[iterator] = {}
-            iterator += 1
-            iterator %= InvThread.size
+                        connection.writeQueue.put(protocol.CreatePacket('inv', \
+                                addresses.encodeVarint(len(hashes)) + "".join(hashes)))
+            invQueue.iterate()
             self.stop.wait(1)
-
-    def holdHash(self, stream, hashId):
-        i = random.randrange(0, InvThread.size)
-        if stream not in self.collectionOfInvs[i]:
-            self.collectionOfInvs[i][stream] = []
-        self.collectionOfInvs[i][stream].append(hashId)
-
-    def hasHash(self, hashId):
-        for streamlist in self.collectionOfInvs:
-            for stream in streamlist:
-                if hashId in streamlist[stream]:
-                    return True
-        return False
-
-    def hashCount(self):
-        retval = 0
-        for streamlist in self.collectionOfInvs:
-            for stream in streamlist:
-                retval += len(streamlist[stream])
-        return retval
-
-    def close(self):
-        self.shutdown = True
