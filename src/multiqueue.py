9b111c11d2d70c71e660f5ed0d5de4779f8ef20c
Peter Surda <surda@economicsofbitcoin.com>
2017-06-27 13:25:12 +0200

    New class multiqueue
    - to be used for invthread and addthread
    - updated invthread for multiqueue

---
+++ src/multiqueue.py
@@ -0,0 +1,35 @@
+from collections import deque
+import Queue
+import random
+
+class MultiQueue(Queue.Queue):
+    defaultQueueCount = 10
+    def __init__(self, maxsize=0, count=0):
+        if not count:
+            self.queueCount = MultiQueue.defaultQueueCount
+        else:
+            self.queueCount = count
+        Queue.Queue.__init__(self, maxsize)
+
+    # Initialize the queue representation
+    def _init(self, maxsize):
+        self.iter = 0
+        self.queues = []
+        for i in range(self.queueCount):
+            self.queues.append(deque())
+
+    def _qsize(self, len=len):
+        return len(self.queues[self.iter])
+
+    # Put a new item in the queue
+    def _put(self, item):
+        #self.queue.append(item)
+        i = random.randrange(0, self.queueCount) 
+        self.queues[i].append((item))
+
+    # Get an item from the queue
+    def _get(self):
+        return self.queues[self.iter].popleft()
+
+    def iterate(self):
+        self.iter = (self.iter + 1) % self.queueCount
--- src/queues.py
+++ src/queues.py
@@ -1,12 +1,15 @@
 import Queue
+
 from class_objectProcessorQueue import ObjectProcessorQueue
+from multiqueue import MultiQueue
 
 workerQueue = Queue.Queue()
 UISignalQueue = Queue.Queue()
 addressGeneratorQueue = Queue.Queue()
 # receiveDataThreads dump objects they hear on the network into this queue to be processed.
 objectProcessorQueue = ObjectProcessorQueue()
-invQueue = Queue.Queue()
+invQueue = MultiQueue()
+addrQueue = MultiQueue()
 portCheckerQueue = Queue.Queue()
 peerDiscoveryQueue = Queue.Queue()
 apiAddressGeneratorReturnQueue = Queue.Queue(
