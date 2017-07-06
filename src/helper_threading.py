4536e44b8c3004710a7f3968b8cbc462df70f4d2
Peter Surda <surda@economicsofbitcoin.com>
2017-07-06 19:35:40 +0200

    Thread names propagate to system
    - the thread names should now show up in the monitoring tools of
    operating systems (tested on linux)

--- src/helper_threading.py
+++ src/helper_threading.py
@@ -1,4 +1,17 @@
 import threading
+try:
+    import prctl
+    def set_thread_name(name): prctl.set_name(name)
+
+    def _thread_name_hack(self):
+        set_thread_name(self.name)
+        threading.Thread.__bootstrap_original__(self)
+
+    threading.Thread.__bootstrap_original__ = threading.Thread._Thread__bootstrap
+    threading.Thread._Thread__bootstrap = _thread_name_hack
+except ImportError:
+    log('WARN: prctl module is not installed. You will not be able to see thread names')
+    def set_thread_name(name): pass
 
 class StoppableThread(object):
     def initStop(self):
