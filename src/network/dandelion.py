5059e120a0865a77c114ee10c0c81543a6326664
Peter Surda <surda@economicsofbitcoin.com>
2017-09-30 13:42:04 +0200

    Dandelion fixes and updates
    - also, randomise the item order in an inv/dinv command

--- src/network/dandelion.py
+++ src/network/dandelion.py
@@ -1,7 +1,6 @@
-import random
+from random import choice
 from threading import RLock
 
-import protocol
 from singleton import Singleton
 
 # randomise routes after 600 seconds
@@ -12,18 +11,21 @@ FLUFF_TRIGGER_TIMEOUT = 300
 class DandelionStems():
     def __init__(self):
         self.stem = {}
+        self.source = {}
         self.timeouts = {}
         self.lock = RLock()
 
-    def add(self, hashId, stems):
+    def add(self, hashId, source, stems):
         with self.lock:
-            self.stem[hashId] = stems
+            self.stem[hashId] = choice(stems)
+            self.source[hashId] = source
             self.timeouts[hashId] = time.time()
 
     def remove(self, hashId):
         with self.lock:
             try:
                 del self.stem[hashId]
+                del self.source[hashId]
                 del self.timeouts[hashId]
             except KeyError:
                 pass
