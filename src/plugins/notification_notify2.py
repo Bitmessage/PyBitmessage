eef74580cd3c5941bc314356e3d9c90cd4055018
Peter Surda <surda@economicsofbitcoin.com>
2017-11-14 23:46:07 +0100

    Typo
    Typo in 89567cecfa7246b717141955eb438540cebfb41e

--- src/plugins/notification_notify2.py
+++ src/plugins/notification_notify2.py
@@ -10,6 +10,6 @@ def connect_plugin(title, subtitle, category, label, icon):
     if not icon:
         icon = 'mail-message-new' if category == 2 else 'pybitmessage'
     connect_plugin.notification.update(title, subtitle, icon)
-    connect_plugin.notification.show.show()
+    connect_plugin.notification.show()
 
 connect_plugin.notification = Notify.Notification.new("Init", "Init")
