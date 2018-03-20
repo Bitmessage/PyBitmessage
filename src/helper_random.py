0cebce23f75c96d55ff07d98377ef5f07ca7dee9
Mahendra <mahendra.g@cisinlabs.com>
2018-03-20 14:28:31 +0530

    added random methods for shuffle, sample, range in helper_random

--- src/helper_random.py
+++ src/helper_random.py
@@ -1,5 +1,5 @@
 import os
-
+import random
 from pyelliptic.openssl import OpenSSL
 
 
@@ -9,3 +9,35 @@ def randomBytes(n):
         return os.urandom(n)
     except NotImplementedError:
         return OpenSSL.rand(n)
+
+
+def randomshuffle(population):
+    """Method randomShuffle.
+
+    shuffle the sequence x in place.
+    shuffles the elements in list in place,
+    so they are in a random order.
+    """
+    return random.shuffle(population)
+
+
+def randomsample(population, k):
+    """Method randomSample.
+
+    return a k length list of unique elements
+    chosen from the population sequence.
+    Used for random sampling
+    without replacement
+    """
+    return random.sample(population, k)
+
+
+def randomrandrange(x, y):
+    """Method randomRandrange.
+
+    return a randomly selected element from
+    range(start, stop). This is equivalent to
+    choice(range(start, stop)),
+    but doesnt actually build a range object.
+    """
+    return random.randrange(x, y)
