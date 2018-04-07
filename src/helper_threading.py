e6e4c66010271c0056c5b841d6f621a97ef20125
Mahendra <mahendra.g@cisinlabs.com>
2018-04-07 15:14:43 +0530

    helper_threading formating

--- src/helper_threading.py
+++ src/helper_threading.py
@@ -1,9 +1,13 @@
+"""Helper threading perform all the threading operations."""
+
 from contextlib import contextmanager
 import threading
 
 try:
     import prctl
-    def set_thread_name(name): prctl.set_name(name)
+
+    def set_thread_name(name):
+        prctl.set_name(name)
 
     def _thread_name_hack(self):
         set_thread_name(self.name)
@@ -12,16 +16,19 @@ try:
     threading.Thread.__bootstrap_original__ = threading.Thread._Thread__bootstrap
     threading.Thread._Thread__bootstrap = _thread_name_hack
 except ImportError:
-    def set_thread_name(name): threading.current_thread().name = name
+    def set_thread_name(name):
+        threading.current_thread().name = name
+
 
 class StoppableThread(object):
     def initStop(self):
         self.stop = threading.Event()
         self._stopped = False
-        
+
     def stopThread(self):
         self._stopped = True
         self.stop.set()
+
 
 class BusyError(threading.ThreadError):
     pass
