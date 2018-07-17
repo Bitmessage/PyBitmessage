2d218a47d97b7810222ae174e5c72d681e63b523
KM-200 <39034780+KM-200@users.noreply.github.com>
2018-07-17 12:03:30 +0200

    no spaces anymore

--- src/depends.py
+++ src/depends.py
@@ -363,7 +363,7 @@ def check_curses():
     import subprocess
 
     try:
-        subprocess.check_call(       "dialog")
+        subprocess.check_call("dialog")
         #subprocess.check_call('which dialog') # no longer works
     except subprocess.CalledProcessError:
         logger.error(
