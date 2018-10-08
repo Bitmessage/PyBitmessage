5572e6b3a1eba2fa78e3b18ba081bed589ab246b
Dmitri Bogomolov <4glitch@gmail.com>
2018-10-08 12:38:41 +0300

    Suppress pylint warning about 'relative import'

--- src/bitmessagemain.py
+++ src/bitmessagemain.py
@@ -367,7 +367,7 @@ class Main:
                         time.time() - state.last_api_response >= 30):
                     self.stop()
         elif not state.enableGUI:
-            from tests import core as test_core
+            from tests import core as test_core  # pylint: disable=relative-import
             test_core_result = test_core.run()
             state.enableGUI = True
             self.stop()
