46d8576a64b2b6c9fa3e77a683a83a4a7d70d9f1
Dmitri Bogomolov <4glitch@gmail.com>
2019-04-24 14:40:25 +0300

    Suppressed pylint relative-import and pycodestyle E402 in depends

--- src/depends.py
+++ src/depends.py
@@ -190,7 +190,7 @@ def try_import(module, log_extra=False):
 def check_ripemd160():
     """Check availability of the RIPEMD160 hash function"""
     try:
-        from fallback import RIPEMD160Hash  # pylint: disable=relative-import
+        from fallback import RIPEMD160Hash
     except ImportError:
         return False
     return RIPEMD160Hash is not None
