a86e43c108c4117472868f2bcecdb0f4d20df31e
lakshyacis <lakshya.p@cisinlabs.com>
2019-09-23 16:09:19 +0530

    sound_gstreamer pylint fixes

--- src/plugins/sound_gstreamer.py
+++ src/plugins/sound_gstreamer.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
-
+"""
+src/plugins/sound_gstreamer.py
+===================================
+"""
 import gi
 gi.require_version('Gst', '1.0')
 from gi.repository import Gst  # noqa: E402
@@ -9,6 +12,7 @@ _player = Gst.ElementFactory.make("playbin", "player")
 
 
 def connect_plugin(sound_file):
+    """Entry point for sound file"""
     _player.set_state(Gst.State.NULL)
     _player.set_property("uri", "file://" + sound_file)
     _player.set_state(Gst.State.PLAYING)
