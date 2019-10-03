ac7a6f750e4fcdf3226e22f1d75894a35dd0a773
mahendar7 <kolpulamahendar@gmail.com>
2019-10-03 13:35:49 +0530

    print statements converted from python 2 to python 3

--- build/compiletest.py
+++ build/compiletest.py
@@ -19,5 +19,5 @@ for filename in matches:
         if 'win' in sys.platform:
             ctypes.windll.user32.MessageBoxA(0, traceback.format_exc(), "Exception in " + filename, 1)
         else:
-            print "Exception in %s: %s" % (filename, traceback.format_exc())
+            print ("Exception in %s: %s" % (filename, traceback.format_exc()))
         sys.exit(1)
