ece3005f42c7f3595f46ed290a6a6da9191a7a21
lakshyacis <lakshya.p@cisinlabs.com>
2019-10-09 18:57:51 +0530

    helper_sent pylint fixes

--- src/helper_sent.py
+++ src/helper_sent.py
@@ -1,6 +1,9 @@
 """
-Insert operation into sent table
+Insert values into sent table
 """
 
+from helper_sql import *
+
 def insert(t):
+    """Perform an insert into the `sent` table"""
     sqlExecute('''INSERT INTO sent VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', *t)
