9b34e56548fdf6763b322b21aca33932b4e3d864
ekta-v <ekta.v@cisinlabs.com>
2019-11-26 16:05:15 +0530

    Updated HMAC Algo

--- src/tests/test_blindsig.py
+++ src/tests/test_blindsig.py
@@ -1,6 +1,7 @@
 import os
 import unittest
-
+import hashlib
+import hmac
 from ctypes import cast, c_char_p
 import sys
 sys.path.insert(1, '/home/cis/ektarepo/PyBitmessage/src/pyelliptic')
@@ -9,14 +10,36 @@ from openssl import OpenSSL
 import msgpack
 signer_obj_list = []
 msgPubKey = []
+msg_list = []
+signature_list = []
+hash_algo = "sha256"
+import base64
 
 class TestBlindSig(unittest.TestCase):
+    def hash(message_Value):
+        H = hashlib.new(hash_algo)
+        H.update(message_Value)
+        b_hash = H.digest()
+        return b_hash
+
+    def verifySerialize(self,level):
+        level = level - 1
+        i = level
+        while i >= 0:
+            if i is 0:
+                verifier_obj = ECCBlind(pubkey=signer_obj_list[0].pubkey)
+            else:
+                verifier_obj = ECCBlind(pubkey=signer_obj_list[i - 1].pubkey)
+            self.assertTrue(verifier_obj.verify(msg_list[i], signature_list[i]))
+            msg = msgpack.unpackb(msg_list[i])
+            print("Unpacked msg at level : ",i,"is ::",msg)
+            i = i - 1
+        print("Msg at line 62 is ",msg)
     def test_blind_sig(self):
         level = 0
-        while level < 5:
+        while level <= 4:
             signer_obj = ECCBlind()
             signer_obj_list.append(signer_obj)
-            
             if level == 0:
                 signer_obj = signer_obj_list[0]              
             else:
@@ -24,8 +47,10 @@ class TestBlindSig(unittest.TestCase):
 
             point_r = signer_obj.signer_init()
             msgPubKey.append(signer_obj.pubkey)
-            msg = msgpack.packb(msgPubKey)
-           
+            hash_value = hash(msgPubKey[level])
+            msg = msgpack.packb(hash_value)
+            msg_list.append(msg)
+            print("Msg at ",level, "ieration is",msg_list[level])
             requester_obj = ECCBlind(pubkey=signer_obj.pubkey)
             msg_blinded = requester_obj.create_signing_request(point_r, msg)
 
@@ -33,8 +58,8 @@ class TestBlindSig(unittest.TestCase):
             OpenSSL.BN_bn2bin(msg_blinded, msg_blinded_str)
 
             signature_blinded = signer_obj.blind_sign(msg_blinded)
-
             signature = requester_obj.unblind(signature_blinded)
+            signature_list.append(signature)
 
             signature_blinded_str = OpenSSL.malloc(0,
                                                            OpenSSL.BN_num_bytes(
@@ -44,16 +69,8 @@ class TestBlindSig(unittest.TestCase):
             OpenSSL.BN_bn2bin(signature[0], signature_str)
             self.assertNotEqual(cast(signature_str, c_char_p).value,
                                         cast(signature_blinded_str, c_char_p).value)
-
-            verifier_obj = ECCBlind(pubkey=signer_obj.pubkey)
-                
-            self.assertTrue(verifier_obj.verify(msg, signature))
-            msg = msgpack.unpackb(msg)
-            
-
             level = level + 1
-
-        print("Msg at line 62 is ",msg)
+        self.verifySerialize(level)
 obj = TestBlindSig('test_blind_sig')
 obj.test_blind_sig()
 
