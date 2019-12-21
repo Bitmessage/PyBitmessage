Bitmessage/PyBitmessage

commit 9923e972797738606235f3817062eed4dd8d8d98

class_singleWorker quality fixes

src/class_singleWorker.py
@@ -1,6 +1,5 @@
 """
-src/class_singleWorker.py
-=========================
+Thread for performing PoW
 """
 # pylint: disable=protected-access,too-many-branches,too-many-statements,no-self-use,too-many-lines,too-many-locals
 
@@ -468,8 +467,8 @@ class singleWorker(StoppableThread):
     def sendOnionPeerObj(self, peer=None):
         """Send onionpeer object representing peer"""
         if not peer:  # find own onionhostname
-            for peer in state.ownAddresses:
-                if peer.host.endswith('.onion'):
+            for peer_ in state.ownAddresses:
+                if peer_.host.endswith('.onion'):
                     break
             else:
                 return

commit 5a35de6bca4385431905be5a59f004f7bfd2896c

Fix sendOnionPeerObj() broken in 9923e97

src/class_singleWorker.py
@@ -467,8 +467,8 @@ class singleWorker(StoppableThread):
     def sendOnionPeerObj(self, peer=None):
         """Send onionpeer object representing peer"""
         if not peer:  # find own onionhostname
-            for peer_ in state.ownAddresses:
-                if peer_.host.endswith('.onion'):
+            for peer in state.ownAddresses:
+                if peer.host.endswith('.onion'):
                     break
             else:
                 return