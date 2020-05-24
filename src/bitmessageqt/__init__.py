Bitmessage/PyBitmessage

commit 045a2ef4436ef20be1baa9caa69934460f19e881

Update messagelist also if search line cleared

src/bitmessageqt/__init__.py
@@ -3884,14 +3884,16 @@ class MyForm(settingsmixin.SMainWindow):
 
     def inboxSearchLineEditUpdated(self, text):
         # dynamic search for too short text is slow
-        if len(str(text)) < 3:
+        text = str(text)
+        if 0 < len(text) < 3:
             return
         messagelist = self.getCurrentMessagelist()
         searchOption = self.getCurrentSearchOption()
         if messagelist:
             account = self.getCurrentAccount()
             folder = self.getCurrentFolder()
-            self.loadMessagelist(messagelist, account, folder, searchOption, str(text))
+            self.loadMessagelist(
+                messagelist, account, folder, searchOption, text)
 
     def inboxSearchLineEditReturnPressed(self):
         logger.debug("Search return pressed")

commit c69decaab319ec99b5a2fa22c754415696542adb

Fix unicode bug introduced in 045a2ef

src/bitmessageqt/__init__.py
@@ -3953,7 +3953,7 @@ class MyForm(settingsmixin.SMainWindow):
 
     def inboxSearchLineEditUpdated(self, text):
         # dynamic search for too short text is slow
-        text = str(text)
+        text = text.toUtf8()
         if 0 < len(text) < 3:
             return
         messagelist = self.getCurrentMessagelist()