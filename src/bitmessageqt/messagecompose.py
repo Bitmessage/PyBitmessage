035fac1fc5e7900146230055627f62a23e7f0686
coolguy-cell <kalap697@gmail.com>
2020-06-05 17:13:42 +0530

    fixed CQ for bitmessageqt.messagecompose module

--- src/bitmessageqt/messagecompose.py
+++ src/bitmessageqt/messagecompose.py
@@ -1,23 +1,37 @@
+"""
+Message editor with a wheel zoom functionality
+"""
+# pylint: disable=bad-continuation
+
 from PyQt4 import QtCore, QtGui
 
+
 class MessageCompose(QtGui.QTextEdit):
-    
-    def __init__(self, parent = 0):
+    """Editor class with wheel zoom functionality"""
+    def __init__(self, parent=0):
         super(MessageCompose, self).__init__(parent)
-        self.setAcceptRichText(False) # we'll deal with this later when we have a new message format
+        self.setAcceptRichText(False)
         self.defaultFontPointSize = self.currentFont().pointSize()
-    
+
     def wheelEvent(self, event):
-        if (QtGui.QApplication.queryKeyboardModifiers() & QtCore.Qt.ControlModifier) == QtCore.Qt.ControlModifier and event.orientation() == QtCore.Qt.Vertical:
+        """Mouse wheel scroll event handler"""
+        if (
+            QtGui.QApplication.queryKeyboardModifiers() & QtCore.Qt.ControlModifier
+        ) == QtCore.Qt.ControlModifier and event.orientation() == QtCore.Qt.Vertical:
             if event.delta() > 0:
                 self.zoomIn(1)
             else:
                 self.zoomOut(1)
             zoom = self.currentFont().pointSize() * 100 / self.defaultFontPointSize
-            QtGui.QApplication.activeWindow().statusBar().showMessage(QtGui.QApplication.translate("MainWindow", "Zoom level %1%").arg(str(zoom)))
+            QtGui.QApplication.activeWindow().statusBar().showMessage(
+                QtGui.QApplication.translate("MainWindow", "Zoom level %1%").arg(
+                    str(zoom)
+                )
+            )
         else:
             # in QTextEdit, super does not zoom, only scroll
             super(MessageCompose, self).wheelEvent(event)
 
     def reset(self):
+        """Clear the edit content"""
         self.setText('')
