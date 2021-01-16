27383ff63516724f3a807aefd30f3b9aff34183a
navjot <navjot.g@cisinlabs.com>
2021-01-17 00:28:40 +0530

    test case for openclpow module

---
+++ src/tests/test_openclpow.py
@@ -0,0 +1,28 @@
+"""
+Tests for openclpow module
+"""
+import hashlib
+import unittest
+
+import pybitmessage.openclpow as openclpow
+
+from struct import pack, unpack
+
+
+class TestOpenClPow(unittest.TestCase):
+    """
+    Main opencl test case
+    """
+
+    @unittest.skipIf(not openclpow.libAvailable,
+                     "pyopencl is not installed")
+    def test_openclpow(self):
+        """Check the working of openclpow module"""
+        openclpow.initCL()
+        target_ = 54227212183
+        initialHash = ("3758f55b5a8d902fd3597e4ce6a2d3f23daff735f65d9698c270987f4e67ad590"
+                       "b93f3ffeba0ef2fd08a8dc2f87b68ae5a0dc819ab57f22ad2c4c9c8618a43b3").decode("hex")
+        nonce = openclpow.do_opencl_pow(initialHash.encode("hex"), target_)
+        trialValue, = unpack(
+            '>Q', hashlib.sha512(hashlib.sha512(pack('>Q', nonce) + initialHash).digest()).digest()[0:8])
+        self.assertLess((nonce - trialValue), target_)
