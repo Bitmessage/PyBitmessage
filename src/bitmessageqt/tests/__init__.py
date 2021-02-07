552733778b11844893db338e52baac54ddaaf285
Dmitri Bogomolov <4glitch@gmail.com>
2021-02-07 21:15:59 +0200

    Move addressbook test to bitmessageqt.tests because it uses Qt

--- src/bitmessageqt/tests/__init__.py
+++ src/bitmessageqt/tests/__init__.py
@@ -1,6 +1,7 @@
 """bitmessageqt tests"""
 
+from addressbook import TestAddressbook
 from main import TestMain, TestUISignaler
 from support import TestSupport
 
-__all__ = ["TestMain", "TestSupport", "TestUISignaler"]
+__all__ = ["TestAddressbook", "TestMain", "TestSupport", "TestUISignaler"]
