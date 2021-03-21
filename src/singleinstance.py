2a769ab8530496a65862c559f85a868e8033b8f4
Ilja Honkonen <ilja.honkonen@protonmail.ch>
2021-03-21 18:48:00 +0200

    Update prints

--- src/singleinstance.py
+++ src/singleinstance.py
@@ -75,7 +75,7 @@ class singleinstance(object):
                     fcntl.lockf(self.fp, fcntl.LOCK_EX | fcntl.LOCK_NB)
                 self.lockPid = os.getpid()
             except IOError:
-                print 'Another instance of this application is already running'
+                print('Another instance of this application is already running')
                 sys.exit(-1)
             else:
                 pidLine = "%i\n" % self.lockPid
@@ -99,7 +99,7 @@ class singleinstance(object):
                 pass
 
             return
-        print "Cleaning up lockfile"
+        print("Cleaning up lockfile")
         try:
             if sys.platform == 'win32':
                 if hasattr(self, 'fd'):
