9855d173450639d6f468bf45707c92732922dd91
cis-kuldeep <kuldeep.k@cisinlabs.com>
2021-04-28 23:41:12 +0530

    Added Dockerfile for Flatpak repo-server & Build instructions under docs/ dir

---
+++ packages/flatpak/server.py
@@ -0,0 +1,10 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+@app.route("/repo/<path:path>")
+def static_dir(path):
+    return send_from_directory("repo", path)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
