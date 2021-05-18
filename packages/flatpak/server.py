4fed8711d75f8b2df664ece612d83f118518f009
cis-kuldeep <kuldeep.k@cisinlabs.com>
2021-05-18 15:09:42 +0530

    fixed code quality

--- packages/flatpak/server.py
+++ packages/flatpak/server.py
@@ -2,9 +2,12 @@ from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
+
 @app.route("/repo/<path:path>")
 def static_dir(path):
+    """Return the pathname of the directory."""
     return send_from_directory("repo", path)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
