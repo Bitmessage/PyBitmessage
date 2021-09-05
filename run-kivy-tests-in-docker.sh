53fd0e825d8581159cf3af0c66da701e381b1d8c
Dmitri Bogomolov <4glitch@gmail.com>
2021-09-05 20:41:43 +0300

    Move additional dockerfiles into packages/docker

--- run-kivy-tests-in-docker.sh
+++ run-kivy-tests-in-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t pybm-kivy-travis-bionic -f Dockerfile.kivy-travis .
+docker build -t pybm-kivy-travis-bionic -f packages/docker/Dockerfile.kivy-travis .
 docker run pybm-kivy-travis-bionic
--- run-tests-in-docker.sh
+++ run-tests-in-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t pybm-travis-bionic -f Dockerfile.travis .
+docker build -t pybm-travis-bionic -f packages/docker/Dockerfile.travis .
 docker run pybm-travis-bionic
