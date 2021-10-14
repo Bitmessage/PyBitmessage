28fe3f9f28bfdff2412cd8dbc02338a8f0b4df5d
shekhar-cis <shekhar.c@cisinlabs.com>
2021-10-14 16:54:11 +0530

    Payment test case[added attribute to check state of sub tabs]

--- src/bitmessagekivy/tests/test_network_screen.py
+++ src/bitmessagekivy/tests/test_network_screen.py
@@ -28,7 +28,14 @@ class NetworkStatusScreen(TeleniumTestProcess):
         self.cli.wait_click('//NavigationItem[@text=\"Network status\"]', timeout=5)
         # checking current screen
         self.assertExists("//ScreenManager[@current=\"networkstat\"]", timeout=5)
+        # Checking the state of "Total Connection" tab
+        self.assertExists(
+            '//NetworkStat/MDTabs[0]//MDTabsLabel[@text=\"Total connections\"][@state=\"down\"]', timeout=3)
+        # Checking the state of "Processes" tab
+        self.assertExists('//NetworkStat/MDTabs[0]//MDTabsLabel[@text=\"Processes\"][@state=\"normal\"]', timeout=3)
+        # Checking the "Tab" is rendered
+        self.assertExists('//NetworkStat/MDTabs[0]//MDTabsLabel[@text=\"Processes\"]', timeout=4)
         # Clicking on Processes Tab
-        self.cli.wait_click('//NetworkStat/MDTabs[0]//MDTabsLabel[@text=\"Processes\"]', timeout=3)
-        # this is for checking current sub-tab
-        self.assertTrue('//NetworkStat/RelativeLayout[1]/Tab[@disabled]', 'False')
+        self.cli.wait_click('//NetworkStat/MDTabs[0]//MDTabsLabel[@text=\"Processes\"]', timeout=4)
+        # Checking the state of "Processes" tab
+        self.assertExists('//NetworkStat/MDTabs[0]//MDTabsLabel[@text=\"Processes\"][@state=\"normal\"]', timeout=3)
