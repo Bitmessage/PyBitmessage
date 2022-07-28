e14a5d3cac30441fd709c85c20c8f672a16bf15d
jd-2205 <jd2205@tutanota.com>
2022-07-28 04:00:47 +0300

    Include test_ecc into the bundle

--- src/pyelliptic/tests/__init__.py
+++ src/pyelliptic/tests/__init__.py
@@ -2,7 +2,8 @@ import sys
 
 if getattr(sys, 'frozen', None):
     from test_arithmetic import TestArithmetic
+    from test_ecc import TestECC
     from test_blindsig import TestBlindSig
     from test_openssl import TestOpenSSL
 
-    __all__ = ["TestArithmetic", "TestBlindSig", "TestOpenSSL"]
+    __all__ = ["TestArithmetic", "TestBlindSig", "TestECC", "TestOpenSSL"]
