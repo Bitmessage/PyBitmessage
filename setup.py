b1aea586ea726239a6b127b5842a357647ec6fa6
shekhar-cis <shekhar.c@cisinlabs.com>
2022-08-24 16:54:48 +0530

    Add bitmessagekivy inside packages

--- setup.py
+++ setup.py
@@ -72,11 +72,11 @@ if __name__ == "__main__":
         'pybitmessage.network',
         'pybitmessage.plugins',
         'pybitmessage.pyelliptic',
-        'pybitmessage.storage',
+        'pybitmessage.storage'
     ]
 
     if sys.version_info[0] == 3:
-        packages.append('pybitmessage.bitmessagekivy')
+        packages.extend(['pybitmessage.bitmessagekivy', 'pybitmessage.bitmessagekivy.baseclass'])
 
     # this will silently accept alternative providers of msgpack
     # if they are already installed
