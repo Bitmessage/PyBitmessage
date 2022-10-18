b574529c4978629b1056df0cd14c90dfbf44775b
Mohammad Osama Khan <mohammad.o@cisinlabs.com>
2022-10-18 19:17:31 +0530

    Add Kivy Settings screen

---
+++ src/bitmessagekivy/baseclass/settings.py
@@ -0,0 +1,10 @@
+# pylint: disable=unused-argument, no-name-in-module, too-few-public-methods
+"""
+Settings screen UI
+"""
+
+from kivy.uix.screenmanager import Screen
+
+
+class Setting(Screen):
+    """Setting Screen for kivy Ui"""
