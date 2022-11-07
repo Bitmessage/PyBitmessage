beadae84f0acbcbce85ef725d27fe310f58cf65c
osamacis <mohammad.o@cisinlabs.com>
2022-11-07 20:16:49 +0530

    Addes load sent list for send messages

--- src/bitmessagekivy/baseclass/sent.py
+++ src/bitmessagekivy/baseclass/sent.py
@@ -25,6 +25,23 @@ class Sent(Screen):
         """Association with the screen"""
         super(Sent, self).__init__(*args, **kwargs)
         self.kivy_state = kivy_state_variables()
+
         if self.kivy_state.selected_address == '':
             if App.get_running_app().identity_list:
                 self.kivy_state.selected_address = App.get_running_app().identity_list[0]
+
+    def init_ui(self, dt=0):
+        """Clock Schdule for method sent accounts"""
+        self.loadSent()
+        print(dt)
+
+    def set_defaultAddress(self):
+        """Set default address"""
+        if self.kivy_state.selected_address == "":
+            if self.kivy_running_app.identity_list:
+                self.kivy_state.selected_address = self.kivy_running_app.identity_list[0]
+
+    def loadSent(self, where="", what=""):
+        """Load Sent list for Sent messages"""
+        self.set_defaultAddress()
+        self.account = self.kivy_state.selected_address
