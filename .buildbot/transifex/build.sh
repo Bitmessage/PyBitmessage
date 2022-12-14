bddc53ca81af165d762093511037a25a1666177e
shekhar-cis <shekhar.c@cisinlabs.com>
2022-12-14 18:04:09 +0530

    Add build.sh and script for po mo

--- .buildbot/transifex/build.sh
+++ .buildbot/transifex/build.sh
@@ -1,1 +1,12 @@
 #!/bin/sh
+echo "Script executed from1: ${PWD}"
+
+mkdir -p ../out
+
+cd pybitmessage/bitmessagekivy
+
+xgettext -Lpython --output=../../../out/messages.pot mpybit.py main.kv ./baseclass/*.py ./kv/*.kv
+
+curl https://www.transifex.com/bitmessage-project/pybitmessage/upload_source/$BUILDBOT_JOBID
+
+# curl https://www.transifex.com/bitmessage-project/pybitmessage-test/$BUILDBOT_JOBID
