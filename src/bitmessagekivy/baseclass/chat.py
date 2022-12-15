255cffd06190faaf7628bb50e5b74fc49da202ef
osamacis <mohammad.o@cisinlabs.com>
2022-12-15 20:01:41 +0530

    Add Chat screen

---
+++ src/bitmessagekivy/baseclass/chat.py
@@ -0,0 +1,11 @@
+# pylint: disable=import-error, no-name-in-module, too-few-public-methods, too-many-ancestors
+
+'''
+   Chats are managed in this screen
+'''
+
+from kivy.uix.screenmanager import Screen
+
+
+class Chat(Screen):
+    """Chat Screen class for kivy Ui"""
