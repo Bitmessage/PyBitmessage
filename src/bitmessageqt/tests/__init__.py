21bef1058ddd6b8f4d7f9f281ade69243e7b027c
Lee Miller <lee.miller@tutanota.com>
2024-05-14 17:38:43 +0300

    Improve the base class for bitmessageqt test cases

--- src/bitmessageqt/tests/__init__.py
+++ src/bitmessageqt/tests/__init__.py
@@ -1,9 +1,9 @@
 """bitmessageqt tests"""
 
-from addressbook import TestAddressbook
-from main import TestMain, TestUISignaler
-from settings import TestSettings
-from support import TestSupport
+from .addressbook import TestAddressbook
+from .main import TestMain, TestUISignaler
+from .settings import TestSettings
+from .support import TestSupport
 
 __all__ = [
     "TestAddressbook", "TestMain", "TestSettings", "TestSupport",
