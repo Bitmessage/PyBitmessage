871aea97fc69ffb07ee0dba6a95454ea7badb1a6
Peter Surda <peter@bitmessage.at>
2026-05-29 10:56:43 +0800

    Devcontainer updates
    - remove obsolete intellicode
    - terminal font change
    - add PyQt4 stubs for linter/autocomplete

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,6 +3,13 @@
 pip3 install -r requirements.txt
 pip3 install -r kivy-requirements.txt
 
+# PyQt4 stubs, for better linter/autocomplete
+git clone https://github.com/TheKewlStore/PyQt4-Stubs.git /tmp/pyqt4-stubs
+P2DIR=$(python2 -c "import site; print(site.getusersitepackages())")/
+P3DIR=$(python3 -c "import site; print(site.getusersitepackages())")/
+cp -r /tmp/pyqt4-stubs/* "${P2DIR}"
+cp -r /tmp/pyqt4-stubs/* "${P3DIR}"
+
 # Linter tools needed by the VS Code extensions (ms-python.flake8,
 # ms-python.pylint, nwgh.bandit).  The apt-installed system packages
 # are not visible to the VS Code Python interpreter.
